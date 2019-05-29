4a26d565d6fbd00a23c140a693235cae42e1af44
huangyongtao <huangyongtao@hangsheng.com.cn>
2019-05-29 09:14:24 +0800

    2019-5-29 base

---
+++ joshua_notebook/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: joshua
+@license: 
+@contact: hytmailforoffice@163.com
+@software: 
+@file: test.py
+@time: 2019/5/28 上午9:47
+@brief:
+"""
+
+list = [0, 1, 2, 3, 4, 5, 5, 5]
+list_5 = (list == 5)
+print(list_5)
